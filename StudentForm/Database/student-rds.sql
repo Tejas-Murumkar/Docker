1795fc709f2043ea7f475b97921baafe757b33bc
Tejas-Murumkar <104541417+Tejas-Murumkar@users.noreply.github.com>
2024-07-15 16:23:40 +0530

    Create student-rds.sql

---
+++ StudentForm/Database/student-rds.sql
@@ -0,0 +1,11 @@
+create database studentapp;
+use studentapp;
+CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
+        student_name VARCHAR(100) NOT NULL,
+    student_addr VARCHAR(100) NOT NULL,
+        student_age VARCHAR(3) NOT NULL,
+        student_qual VARCHAR(20) NOT NULL,
+        student_percent VARCHAR(10) NOT NULL,
+        student_year_passed VARCHAR(10) NOT NULL,
+        PRIMARY KEY (student_id)
+);
